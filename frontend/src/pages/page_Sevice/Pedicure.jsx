4bc764d4c04b2c4f7890aed37f6a951dada4d3d2
TrongNghia <hoangtrongnghia2013@example.com>
2025-09-24 23:47:30 +0700

    MERGE-CODE

---
+++ frontend/src/pages/page_Sevice/Pedicure.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import BookingForm from "../../components/BookingForm"; // Thay đổi đường dẫn nếu cần
+
+export default function Makeup() {
+    return (
+        <>
+            <div className="container-fluid bg-light page-header py-5 mb-5">
+                <div className="container text-center py-5">
+                    <h1 className="display-1">Pedicure</h1>
+                </div>
+            </div>
+
+            {/* Sử dụng component form và truyền tên dịch vụ vào */}
+            <BookingForm serviceName="Pedicure" />
+        </>
+    );
+}
